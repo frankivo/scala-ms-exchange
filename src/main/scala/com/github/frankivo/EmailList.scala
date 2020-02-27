74e46d385fdf3e175a90375272aeb981d62db5e9
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2020-02-27 15:16:48 +0100

    More comments

--- src/main/scala/com/github/frankivo/EmailList.scala
+++ src/main/scala/com/github/frankivo/EmailList.scala
@@ -5,21 +5,48 @@ import java.util.Date
 
 import microsoft.exchange.webservices.data.core.service.item.Item
 
+/**
+ * Helper methods for List[Item].
+ */
 object EmailList {
+
+  /**
+   * Adds methods to List[Item].
+   * @param items List of e-mails.
+   */
   implicit class Filters(items: List[Item]) {
 
+    /**
+     * Find e-mails by partial subject.
+     * @param subject Subject to search for.
+     * @return Filtered list of e-mails.
+     */
     def subjectContains(subject: String): List[Item] = items.filter(_.getSubject.contains(subject))
 
+    /**
+     * Find e-mails by maximum received date.
+     * @param hours Maximum age of mails in hours.
+     * @return Filtered list of e-mails.
+     */
     def maxAge(hours: Int): List[Item] = {
       val maxAge = Date.from(Instant.now.minusSeconds(hours * 60))
       items.filter(_.getDateTimeReceived.after(maxAge))
     }
 
+    /**
+     * Find e-mails by minimum received date.
+     * @param days Minimum age of mails in days.
+     * @return Filtered list of e-mails.
+     */
     def minAge(days: Int): List[Item] = {
       val minAge = Date.from(Instant.now.minus(Period.ofDays(days)))
       items.filter(_.getDateTimeReceived.before(minAge))
     }
 
+    /**
+     * Find e-mails that have at least one attachment.
+     * @return Filtered list of e-mails.
+     */
     def hasAttachment: List[Item] = items.filter(_.getHasAttachments)
   }
 }
