1dd7eaa202d0335ff8ed6f85112d00ca20b42b6d
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2020-02-27 15:13:17 +0100

    Store attachments

--- src/main/scala/com/github/frankivo/Email.scala
+++ src/main/scala/com/github/frankivo/Email.scala
@@ -1,11 +1,41 @@
 package com.github.frankivo
 
+import java.nio.file.Paths
+
+import microsoft.exchange.webservices.data.core.PropertySet
 import microsoft.exchange.webservices.data.core.enumeration.service.DeleteMode
 import microsoft.exchange.webservices.data.core.service.item.Item
+import microsoft.exchange.webservices.data.core.service.schema.ItemSchema
+import microsoft.exchange.webservices.data.property.complex.FileAttachment
+
+import scala.collection.JavaConverters._
 
 object Email {
 
   implicit class Actions(item: Item) {
+    /**
+     * Permanently deletes mail from server.
+     */
     def delete(): Unit = item.delete(DeleteMode.HardDelete)
+
+    /**
+     * Stores all attachments to disk.
+     * @param path File store-location.
+     * @return List of stored files.
+     */
+    def storeAttachments(path: String): List[String] = {
+      val extended = Item.bind(item.getService, item.getId, new PropertySet(ItemSchema.Attachments))
+      extended
+        .getAttachments
+        .asScala
+        .toList
+        .map(a => a.asInstanceOf[FileAttachment])
+        .map(a => {
+          val path = Paths.get(path, a.getName)
+          a.load(path.toString)
+          path.toString
+        })
+    }
   }
+
 }
