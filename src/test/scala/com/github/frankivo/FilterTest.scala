40b541ac9bb81a805ecb3fea0282fb2591e0bd67
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2020-04-09 10:14:47 +0200

    Second test for Subject

--- src/test/scala/com/github/frankivo/FilterTest.scala
+++ src/test/scala/com/github/frankivo/FilterTest.scala
@@ -19,15 +19,16 @@ class FilterTest extends AnyFlatSpec with MockitoSugar with Matchers {
     mails.length should be(1)
     mails.head.getSubject should be("Mail 3")
   }
+
+  "subject filter" should "not find an non-existing subject" in {
     val mail = List[Item](
       mockMail("Mail 1"),
       mockMail("Mail 2"),
       mockMail("Mail 3"),
       mockMail("Mail 4")
-    ).subjectContains("Mail 3")
+    ).subjectContains("Fake mail")
 
-    mail.length should be(1)
-    mail.head.getSubject should be("Mail 3")
+    mail.length should be(0)
   }
 
   def mockMail(subject: String): Item = {
